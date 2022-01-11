71968ccbe252f1c287dafabfb330fb0cc6d8f801
Bhupesh Parab - NeoSoft (Partner <P115552@fullertonindia.local>
2022-01-11 14:17:33 +0530

    changes

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,9 +1,7 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-app.js";
-import { getMessaging } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-messaging.js";
-import { onBackgroundMessage } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-messaging-sw.js";
+importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-messaging.js')
 
-
-const firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyDRVMFWfoV5vgEI82Qy0jbIOT-q9ZyTWTQ",
     authDomain: "csp-fcm.firebaseapp.com",
     projectId: "csp-fcm",
@@ -11,20 +9,21 @@ const firebaseConfig = {
     messagingSenderId: "1063903322339",
     appId: "1:1063903322339:web:72f81dfb12fbb4c991a5d6"
 };
-const firebaseApp = initializeApp(firebaseConfig);
 
-const messaging = getMessaging(firebaseApp);
+var app = firebase.initializeApp(firebaseConfig);
 
-onBackgroundMessage(messaging, (payload) => {
+var messaging = firebase.messaging(app);
+
+messaging.setBackgroundMessageHandler(function (payload) {
     console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    const notification = JSON.parse(payload.data.notification);
     // Customize notification here
-    const notificationTitle = 'Background Message Title';
+    const notificationTitle = notification.title;
     const notificationOptions = {
-        body: 'Background Message body.',
-        icon: '/firebase-logo.png'
+        body: notification.body,
+        icon: notification.icon
     };
 
     self.registration.showNotification(notificationTitle,
         notificationOptions);
 });
-
