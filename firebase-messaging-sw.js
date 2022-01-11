8e7d881f30aed576dbffdec78e5b1f2abc99048c
Bhupesh Parab - NeoSoft (Partner <P115552@fullertonindia.local>
2022-01-11 10:58:23 +0530

    changes

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,29 @@
+importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/8.3.2/firebase-messaging.js')
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDRVMFWfoV5vgEI82Qy0jbIOT-q9ZyTWTQ",
+    authDomain: "csp-fcm.firebaseapp.com",
+    projectId: "csp-fcm",
+    storageBucket: "csp-fcm.appspot.com",
+    messagingSenderId: "1063903322339",
+    appId: "1:1063903322339:web:72f81dfb12fbb4c991a5d6"
+};
+
+var app = firebase.initializeApp(firebaseConfig);
+
+var messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    const notification = JSON.parse(payload.data.notification);
+    // Customize notification here
+    const notificationTitle = notification.title;
+    const notificationOptions = {
+        body: notification.body,
+        icon: notification.icon
+    };
+
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
